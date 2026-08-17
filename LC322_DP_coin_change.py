ee1463dde2a7563f7343d90e4ed7663f5f1f5fda
paragc05 <paragc05@users.noreply.github.com>
2026-08-17 00:14:51 -0400

    name correction

--- LC322_DP_coin_change.py
+++ LC322_DP_coin_change.py
@@ -1,21 +1,26 @@
 #  Time Complexity : O(n)
-#  Space Complexity : O(n)
+#  Space Complexity : O(1)
 #  Did this code successfully run on Leetcode : yes
 #  Any problem you faced while coding this : No
 
-# Approach: built a dp[i] as the minimum coins needed to make amount i, checking every coin for each amount
+# Approach: at each house, choosing max(skip current, rob current + money from i-2) while keeping only the previous two DP values
 
 
-def rob(nums: list[int]) -> int:
-    n = len(nums)
-    if n == 1:
-        return nums[0]
-    prev = nums[0]
-    curr = max(nums[0], nums[1])
+def coinChange(coins: list[int], amount: int) -> int:
+    m = len(coins)
+    n = amount
+    # m rows and n cols
+    dp = [0] * (n + 1)
 
-    for i in range(2, n):
-        temp = curr
-        curr = max(temp, nums[i] + prev)
-        prev = temp
+    for j in range(1, n + 1):
+        dp[j] = 99999
 
-    return curr
+    for i in range(1, m + 1):
+        for j in range(n + 1):
+            # choose case
+            if j >= coins[i - 1]:
+                dp[j] = min(dp[j], dp[j - coins[i - 1]] + 1)
+
+    if dp[n] == 99999:
+        return -1
+    return dp[n]
